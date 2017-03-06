554b5acdd72dc7c1bcd83aa4926e689b7636905f
Janine Vega <osmary.vega@gmail.com>
2017-03-05 22:07:42 -0500

    agregando mas planetas

--- js/main.js
+++ js/main.js
@@ -2,13 +2,17 @@ function pesoPlanetas(){
   var peso = prompt("Ingrese su peso:");
   document.getElementById('pesoTierra').value=peso;
   mercurio = (peso * 2.8/9.8).toFixed(2);
-  venus = (peso * 8.9/9.8).toFixed(2);
-  marte = (peso * 3.7/9.8).toFixed(2);
+  venus   = (peso * 8.9/9.8).toFixed(2);
+  marte   = (peso * 3.7/9.8).toFixed(2);
   jupiter = (peso * 22.9/9.8).toFixed(2);
   saturno = (peso * 9.1/9.8).toFixed(2)
-  urano = (peso * 7.8/9.8).toFixed(2);
+  urano   = (peso * 7.8/9.8).toFixed(2);
   neptuno = (peso * 11.0/9.8).toFixed(2);
-  luna = (peso * 1.6/9.8).toFixed(2)
+  luna    = (peso * 1.6/9.8).toFixed(2);
+  pluton  = (peso * 0.62/9.8).toFixed(2);
+  urano   = (peso * 8.69/9.8).toFixed(2);
+  ceceres = (peso * 0.27/9.8).toFixed(2);
+  titan   = (peso * 1.352/9.8).toFixed(2);
   alert ("Recuerda que el peso y la masa de un cuerpo no son lo mismo, "+
                     "así que dado tu peso en el planeta Tierra, tu peso en otros " +
                     "planetas serán los siguientes:" + "\n" +"\n" +
@@ -19,8 +23,9 @@ function pesoPlanetas(){
                     "En Saturno: " + saturno + "\n" +
                     "En Urano: " + urano + "\n" +
                     "En Neptuno: " + neptuno + "\n" +
-                    "Yapa: " + "\n" +
-                    "En la Luna: " + luna  );
+                    "En la luna de la Tierra: " + luna + "\n" +
+                    "En Urano: " + urano + "\n" +
+                    "En Ceceres: " + ceceres + "\n" +
+                    "En la Luna de Saturno: " + titan  );
 }
-
 pesoPlanetas();
--- js/peso.js
+++ js/peso.js
@@ -28,4 +28,25 @@ window.onload=function calcular(){
   var pesoN = pesoT*(11.0/9.8);
   console.log(pesoN);
   document.getElementById('pesoN').value= pesoN.toFixed(3);
+
+  var pesoL = pesoT*(1.6/9.8);
+  console.log(pesoL);
+  document.getElementById('luna').value= pesoL.toFixed(3);
+
+  var pesopluton = pesoT*(0.62/9.8);
+  console.log(pesopluton );
+  document.getElementById('pluton').value= pesopluton.toFixed(3);
+
+  var pesourano = pesoT*(8.69/9.8);
+  console.log(pesourano);
+  document.getElementById('urano').value= pesourano.toFixed(3);
+
+  var pesoceceres = pesoT*(0.27/9.8);
+  console.log(pesoceceres );
+  document.getElementById('ceceres').value= pesoceceres.toFixed(3);
+
+  var pesotitan = pesoT*(1.352/9.8);
+  console.log(pesotitan);
+  document.getElementById('titan').value= pesotitan.toFixed(3);
+
 }
